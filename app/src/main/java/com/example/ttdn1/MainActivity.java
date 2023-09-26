9a919bd81851df6756a999e4868b12331e23e826
NguyenLongVu03 <117622104+NguyenLongVu03@users.noreply.github.com>
2023-09-26 17:27:20 +0700

    fragment

--- app/src/main/java/com/example/ttdn1/MainActivity.java
+++ app/src/main/java/com/example/ttdn1/MainActivity.java
@@ -1,14 +1,82 @@
 package com.example.ttdn1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.ttdn1.adapter.AdapterViewPager;
+import com.example.ttdn1.fragments.HomeFragment;
+import com.example.ttdn1.fragments.LikeFragment;
+import com.example.ttdn1.fragments.MusicFragment;
+import com.example.ttdn1.fragments.UserFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ViewPager2 viewPager2;
+    ArrayList<Fragment> arrayList = new ArrayList<>();
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewPager2 = findViewById(R.id.pagerMain);
+        bottomNavigationView = findViewById(R.id.bottomNavigate);
+        arrayList.add(new HomeFragment());
+        arrayList.add(new MusicFragment());
+        arrayList.add(new LikeFragment());
+        arrayList.add(new UserFragment());
+
+        AdapterViewPager adapterViewPager = new AdapterViewPager(this, arrayList);
+
+        //setAdapter
+        viewPager2.setAdapter(adapterViewPager);
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                switch (position){
+                    case 0:
+                        bottomNavigationView.setSelectedItemId(R.id.home);
+                        break;
+                    case 1:
+                        bottomNavigationView.setSelectedItemId(R.id.music);
+                        break;
+                    case 2:
+                        bottomNavigationView.setSelectedItemId(R.id.tym);
+                        break;
+                    case 3:
+                        bottomNavigationView.setSelectedItemId(R.id.map);
+                        break;
+                }
+                super.onPageSelected(position);
+            }
+        });
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        viewPager2.setCurrentItem(0);
+                        break;
+                    case R.id.music:
+                        viewPager2.setCurrentItem(1);
+                        break;
+                    case R.id.tym:
+                        viewPager2.setCurrentItem(2);
+                        break;
+                    case R.id.map:
+                        viewPager2.setCurrentItem(3);
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/ttdn1/adapter/AdapterViewPager.java
@@ -0,0 +1,28 @@
+package com.example.ttdn1.adapter;
+
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.ArrayList;
+
+public class AdapterViewPager extends FragmentStateAdapter {
+    ArrayList<Fragment> arrayList;
+    public AdapterViewPager(@NonNull FragmentActivity fragmentActivity, ArrayList<Fragment> arrayList) {
+        super(fragmentActivity);
+        this.arrayList = arrayList;
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        return arrayList.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrayList.size();
+    }
+}
