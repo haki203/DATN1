02e7efc1c3e25d5d6470a2b0101e2df56f998743
tho.nguyen <91511523+haki203@users.noreply.github.com>
2023-09-30 16:39:31 +0700

    Revert "."
    This reverts commit c4e538dda2cef0ae37bff842bbe38a6fbc27048a, reversing
    changes made to b88c8926d7832047cc56e89580ff246572e44ce8.

--- app/src/main/java/com/example/ttdn1/MainActivity.java
+++ app/src/main/java/com/example/ttdn1/MainActivity.java
@@ -2,47 +2,13 @@ package com.example.ttdn1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
-import com.example.ttdn1.authentication.LoginActivity;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-
-    FirebaseAuth auth;
-    Button button;
-    TextView textView;
-    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        auth= FirebaseAuth.getInstance();
-        button = findViewById(R.id.btnLogOut);
-        textView = findViewById(R.id.tvHello);
-        user = auth.getCurrentUser();
-        if (user == null){
-            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-            startActivity(intent);
-            finish();
-        } else {
-            textView.setText(user.getEmail());
-        }
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
     }
 }
