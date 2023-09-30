1e176bc7b919762223f3cc9efe10f4c76ab7db82
tho.nguyen <91511523+haki203@users.noreply.github.com>
2023-09-30 21:05:04 +0700

    .

--- app/src/main/java/com/example/ttdn1/FCMActivity.java
+++ app/src/main/java/com/example/ttdn1/FCMActivity.java
@@ -29,19 +29,6 @@ public class FCMActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fcmactivity);
 //        btn= findViewById(R.id.btn);
-
-
-        /*
-            1: đăng ký trên console.firebase.google.com (enable cloud messaging để lấy server key)
-            2: thêm thư viện và google.json và apply
-            3: thêm thư vien implementation ("com.google.firebase:firebase-messaging:22.0.0")
-            4: tạo FCMNotifierd để gửi api
-            5: tao service để nhận kq từ api
-            6: xu ly thong bao
-
-        */
-
-
 //        btn.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View view) {
@@ -68,72 +55,6 @@ public class FCMActivity extends AppCompatActivity {
 //                //createNotificationChannel2();
 //            }
 //        });
-    }
-
-    private void createNotificationChannel() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = "Thông báo";
-            String description = "Thông báo";
-            int importance = NotificationManager.IMPORTANCE_HIGH;
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
-            channel.setDescription(description);
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
-            NotificationManager notificationManager = this.getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
-        NotificationCompat.BigTextStyle bigTextStyle = new NotificationCompat.BigTextStyle();
-        bigTextStyle.setBigContentTitle("\uD83D\uDCE3\uD83D\uDCE3\uD83D\uDCE3 Đi học nào kẻo kẹt xe!!!");
-        bigTextStyle.bigText("Môn học Android Networking sẽ bắt đầu vào CA 5. Bạn hãy nhớ nhé!");
-        // Tạo thông báo bằng cách sử dụng NotificationCompat.Builder
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(R.drawable.baseline_notifications_none_24) // Icon cho thông báo
-                .setContentTitle("\uD83D\uDCE3\uD83D\uDCE3\uD83D\uDCE3 Đi học nào kẻo kẹt xe!!!") // Tiêu đề của thông báo
-                .setContentText("Môn học Android Networking sẽ bắt đầu vào CA 5. Bạn hãy nhớ nhé!") // Nội dung của thông báo
-                .setStyle(bigTextStyle)
-                .setPriority(NotificationCompat.PRIORITY_HIGH);
-
-        // Hiển thị thông báo
-        NotificationManager notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(/* ID thông báo */ 1, builder.build());
-    }
-    private void createNotificationChannel2() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = "Thông báo";
-            String description = "Thông báo";
-            int importance = NotificationManager.IMPORTANCE_HIGH;
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
-            channel.setDescription(description);
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
-
-        // Intent cho màn hình đầy đủ (full screen) khi thông báo được bật lên
-        Intent fullScreenIntent = new Intent(this, FirebaseStorageActivity.class);
-        PendingIntent fullScreenPendingIntent = PendingIntent.getActivity(this, 0,
-                fullScreenIntent, PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_UPDATE_CURRENT);
-        NotificationCompat.BigTextStyle bigTextStyle = new NotificationCompat.BigTextStyle();
-        bigTextStyle.setBigContentTitle("\uD83D\uDCE3\uD83D\uDCE3\uD83D\uDCE3 Đi học nào kẻo kẹt xe!!!");
-        bigTextStyle.bigText("Môn học Android Networking sẽ bắt đầu vào CA 5. Bạn hãy nhớ nhé!");
-
-        // Tạo thông báo bằng cách sử dụng NotificationCompat.Builder
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(R.drawable.baseline_notifications_none_24) // Icon cho thông báo
-                .setContentTitle("\uD83D\uDCE3\uD83D\uDCE3\uD83D\uDCE3 Đi học nào kẻo kẹt xe!!!") // Tiêu đề của thông báo
-                .setContentText("Môn học Android Networking sẽ bắt đầu vào CA 5. Bạn hãy nhớ nhé!") // Nội dung của thông báo
-                .setStyle(bigTextStyle)
-                .setPriority(NotificationCompat.PRIORITY_HIGH) // Đặt mức độ ưu tiên là HIGH
-                .setFullScreenIntent(fullScreenPendingIntent, true);
-
-        // Hiển thị thông báo
-        NotificationManager notificationManager = getSystemService(NotificationManager.class);
-        notificationManager.notify(/* ID thông báo */ 1, builder.build());
     }
 
 }
